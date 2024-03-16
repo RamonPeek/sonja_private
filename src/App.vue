b676b7bc58a68bf862c62137bbafb3fc0a74b317
RamonPeek <ramonpeek@hotmail.com>
2024-03-16 11:30:17 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MooiChart msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MooiChart from './components/MooiChart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MooiChart
   }
 }
 </script>
@@ -23,4 +22,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style>./components/Chart.vue
