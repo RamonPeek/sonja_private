5f5d9dd6a780b8406803cf940b7e9b7bfd2a0ed2
RamonPeek <ramonpeek@hotmail.com>
2024-05-18 12:39:20 +0200

    Added dist

---
+++ dist/js/about.5a2a8131.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksonja"]=self["webpackChunksonja"]||[]).push([[594],{5282:function(n,u,a){a.r(u),a.d(u,{default:function(){return l}});var s=a(6768);const t={class:"about"},e=(0,s.Lk)("h1",null,"This is an about page",-1),r=[e];function c(n,u){return(0,s.uX)(),(0,s.CE)("div",t,r)}var o=a(1241);const i={},f=(0,o.A)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.5a2a8131.js.map
